e13361161f45fbb8c3082bccfe46c40bc660cdcb
Tu Nombre <amasterjt@email.com>
2026-06-18 22:13:26 +0200

    agregamos un fichero para limpiar el servidor y dejarlo por defecto

--- scripts/clean_server.sh
+++ scripts/clean_server.sh
@@ -9,3 +9,12 @@ sudo deluser --remove-home dev 2>/dev/null
 sudo deluser --remove-home victima 2>/dev/null
 sudo groupdel developers 2>/dev/null
 sudo systemctl restart ssh
+
+
+sudo groupadd developers 2>/dev/null
+sudo useradd -m -s /bin/bash -g developers victima
+echo 'victima:victima123' | sudo chpasswd
+
+sudo mkdir -p /home/victima
+sudo chown victima:developers /home/victima
+sudo chmod 755 /home/victima
